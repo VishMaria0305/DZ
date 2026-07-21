7aef6bff545f3b1aa0da77c9d953b45cbac51fa4
Mari <solomapaloma@yandex.ru>
2026-07-21 08:46:35 +0300

    Добавлены файлы kolco.cpp и kank2.cpp

---
+++ kank2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double a, b;
+    char znak; 
+    while (true) {
+    std::cout << "Введите 1е число:\n";
+    std::cin >> a;
+  
+        std::cout << "Введите знак (+, -, *, /, x - макс, n - мин, w - выход)\n";
+        std::cin >> znak;
+        if (znak == 'w') { //символ для выхода из программы
+            std::cout << "Работа программы завершена.\n";
+            break;
+        }
+
+        std::cout << "Введите 2е число:\n";
+        std::cin >> b;
+
+        switch (znak) {
+        case '+':
+            std::cout << a + b << std::endl;
+            break;
+        case '-':
+            std::cout << a - b << std::endl;
+            break;
+        case '*':
+            std::cout << a * b << std::endl;
+            break;
+        case '/':
+            std::cout << a / b << std::endl;
+            break;
+        case 'x': // максисум
+            if (a > b)
+                std::cout << "Макс:\n" << a << std::endl;
+            else
+                std::cout << "Макс:\n" << b << std::endl;
+            break;
+        case 'n': // минимум
+            if (a < b)
+                std::cout << "Мин:\n" << a << std::endl;
+            else
+                std::cout << "Мин:\n" << b << std::endl;
+            break;
+        default:
+            std::cout << "Неверно\n" << std::endl;
+        }
+    }
+    return 0;
+}
