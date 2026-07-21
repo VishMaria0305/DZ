735cd66243389e1c20a7d3e271c1381f829c2a37
Mari <solomapaloma@yandex.ru>
2026-07-21 20:25:02 +0300

    Добавил задачу про улитку

---
+++ ylitka.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <clocale>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int sm = 15; //15 см в 1 день
+    int n; //задает пользолватель дней
+    int sum = 0; //общая сумма пробега
+    std::cout << "Введите количество дней: ";
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        sum = sum + sm;
+        sm = sm + 2; 
+        
+        
+    
+    }
+ 
+    std::cout << "Общее расстояние: " << sum << std::endl;
+    return 0;
+}
